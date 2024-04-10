884ee76456c6e71c19970861da964d660fbb12a4
rijwan-uddin <rijwan35-653@diu.edu.bd>
2024-04-10 10:42:21 +0600

    update

---
+++ lib/page_four.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PageFour extends StatelessWidget {
+  static const String routeName = 'Four';
+  const PageFour({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page Four'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
